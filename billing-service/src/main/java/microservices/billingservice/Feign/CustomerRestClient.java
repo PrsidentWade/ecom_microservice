874a8e75e8aa83e4cae1daf271f82a5b7d33de25
PrsidentWade <abdoulayewade333@gmail.com>
2026-04-20 22:24:05 +0000

    Fault Tolerence

--- billing-service/src/main/java/microservices/billingservice/Feign/CustomerRestClient.java
+++ billing-service/src/main/java/microservices/billingservice/Feign/CustomerRestClient.java
@@ -1,5 +1,6 @@
 package microservices.billingservice.Feign;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import microservices.billingservice.Model.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "customer-service")
 public interface CustomerRestClient {
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customer-service", fallbackMethod = "getDefaultCustomer")
     Customer getCustomerById(@PathVariable Long id);
+
+    default Customer getDefaultCustomer(Long id, Exception exception){
+        exception.getStackTrace();
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstname("yacibne");
+        customer.setLastname("sarr");
+        customer.setEmail("sarr@gmeial.com");
+        return customer;
+
+    }
 
 }
--- customer-service/src/main/java/microservices/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/microservices/customerservice/CustomerServiceApplication.java
@@ -5,7 +5,7 @@ import microservices.customerservice.Repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
